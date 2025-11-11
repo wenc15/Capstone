42f52209e241cb5d95d6f54e6006e782cb031f3b
jingyaosun8509 <sun250@mcmaster.ca>
2025-11-11 15:10:35 -0500

    Add UI prototype

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,26 @@
+// Capstone/ui/main.js
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { pathToFileURL } = require('url');
+const fs = require('fs');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 400,
-    height: 300,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: false
-    }
+    width: 1000,
+    height: 700,
+    webPreferences: { nodeIntegration: true, contextIsolation: false }
   });
 
-  win.loadFile('index.html');
+  const htmlPath = path.join(__dirname, 'index.html');
+  console.log('Loading:', htmlPath, fs.existsSync(htmlPath));
+  win.loadURL(pathToFileURL(htmlPath).toString());
+
+  //win.webContents.openDevTools({ mode: 'detach' });
+  win.webContents.on('did-fail-load', (_e, code, desc) => {
+    console.error('did-fail-load:', code, desc);
+  });
 }
 
 app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
+app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
